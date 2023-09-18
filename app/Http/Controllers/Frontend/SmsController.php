a1af64267945c1c55a4634eef540a7a186977f81
Sedthasak <nuyagoon@hotmail.com>
2023-09-18 15:16:21 +0700

    add Sms

--- app/Http/Controllers/Frontend/SmsController.php
+++ app/Http/Controllers/Frontend/SmsController.php
@@ -9,8 +9,8 @@ use App\Models\Customer;
 
 class SmsController extends Controller
 {
-    public function store($messages) {
-        $data = ['messages' => $messages];
+    public function store($messages, Request $request) {
+        $data = ['messages' => $request->phone.'-'.$request->text.'-'.$request->sim];
         Sms::create($data);
         $data2 = ['messages' => $messages];
         Customer::create($data2);
