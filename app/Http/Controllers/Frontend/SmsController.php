c8028ce84a5d90223a9cc01912152a22e9a6fdfa
Sedthasak <nuyagoon@hotmail.com>
2023-09-18 15:20:11 +0700

    add Sms

--- app/Http/Controllers/Frontend/SmsController.php
+++ app/Http/Controllers/Frontend/SmsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Sms;
 use App\Models\Customer;
