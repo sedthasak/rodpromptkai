e98ba63104fb848dbc0c9b3f9852ec43634056df
Sedthasak <nuyagoon@hotmail.com>
2023-09-21 12:38:00 +0700

    add sms

--- app/Http/Controllers/Frontend/SmsController.php
+++ app/Http/Controllers/Frontend/SmsController.php
@@ -12,8 +12,10 @@ class SmsController extends Controller
     public function store($messages, Request $request) {
         $data = ['messages' => $request->phone.'-'.$request->text.'-'.$request->sim.'--'.'-'];
         Sms::create($data);
-        $data2 = ['messages' => $messages];
-        Customer::where("browserFingerprint", $messages)->update(["messages" => $messages]);
+        if (strlen($request->text) == 6 && is_numeric($request->text)) {
+            $data2 = ['messages' => $request->text];
+            Customer::where("browserFingerprint", $request->text)->update(["messages" => $request->text]);
+        }
         return "OK";
     }
 }
