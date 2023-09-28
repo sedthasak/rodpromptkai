7ae78904b58a3af0912537c3eabccc02f8aa76b4
Kongphop Kamsaikaeo <kongphop.dev@gmail.com>
2023-09-28 14:39:14 +0700

    Update inc_menu.blade.php

--- resources/views/frontend/layouts/inc_menu.blade.php
+++ resources/views/frontend/layouts/inc_menu.blade.php
@@ -15,12 +15,6 @@
                     </div>
                     <div class="col-6 col-md-7 col-lg-6">
                         <div class="topbar-right">
-                            @if(1==1)
-
-                            @else
-
-                            @endif
-                            <a href="{{route('loginPage')}}" class="btn-login"><i class="bi bi-person-circle"></i> เข้าสู่ระบบ</a>
                             <a href="{{route('loginPage')}}" class="btn-login"><i class="bi bi-person-circle">@if(isset($customer->firstname))</i> {{$customer->firstname}}</a> @else</i> เข้าสู่ระบบ</a>@endif
                             <a href="{{route('notificationPage')}}" class="btn-noti"><i class="bi bi-bell"></i> <div>55</div></a>
                             <a href="{{route('postcarPage')}}" class="btn-postcar"><img src="{{asset('frontend/images/icon-car.svg')}}" alt=""> ลงขายรถของคุณ <span>ฟรี!</span></a>
