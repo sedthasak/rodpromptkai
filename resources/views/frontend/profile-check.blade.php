55e42bc8f6bb537838a0a27dc6a0e42e9c23ba07
Kongphop Kamsaikaeo <kongphop.dev@gmail.com>
2023-11-28 17:40:57 +0700

    Update profile-check.blade.php

--- resources/views/frontend/profile-check.blade.php
+++ resources/views/frontend/profile-check.blade.php
@@ -8,97 +8,7 @@
 
 
 @include('frontend.layouts.inc_profile')	
-<!-- <section class="row">
-    <div class="col-12 page-profile">
-        <div class="container">
-            <div class="row">
-                @include('frontend.layouts.inc-menuprofile-search')
-                <div class="col-12 col-lg-8 col-xl-9">
-                    <div class="desc-pageprofile">
-                        <div class="wraptopic-pageprofile">
-                            <div class="topic-profilepage"><i class="bi bi-circle-fill"></i> รถที่ลงขาย</div>
-                            <button class="show-menuprofile"><i class="bi bi-search"></i>ค้นหารถในบัญชี</button>
-                        </div>
-                        @include('frontend.layouts.inc_menu-mycar')
-                        <div class="item-mycar">
-                            <div class="item-mycar-cover">
-                                <a href="car-detail.php"><figure><img src="{{asset('frontend/images/CAR202304060018_BMW_X5_20230406_101922704_WATERMARK.png')}}" alt=""></figure></a>
-                            </div>
-                            <div class="mycar-detail-mb">
-                                <a href="car-detail.php">
-                                    <div class="mycar-name">2023 BMW X1</div>
-                                    <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                    <div class="mycar-idcar">4กข 8113</div>
-                                </a>
-                            </div>
-                            <div class="item-mycar-detail-check item-mycar-detail">
-                                <div class="row">
-                                    <div class="col-12 col-md-6">
-                                        <a href="car-detail.php">
-                                            <div class="mycar-name">2023 BMW X1</div>
-                                            <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                            <div class="mycar-idcar">4กข 8113</div>
-                                        </a>
-                                    </div>
-                                    <div class="col-12 col-md-6 text-end">
-                                        <div class="mycar-post">วันที่ลงขาย :  16 มิ.ย. 66</div>
-                                        <div class="mycar-price-mb mycar-price">599,000.-</div>
-                                    </div>
-                                </div>
-                                <div class="mycar-boxline">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="mycar-boxprice">
-                                                <div class="mycar-price">599,000.-</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="item-mycar">
-                            <div class="item-mycar-cover">
-                                <a href="car-detail.php"><figure><img src="{{asset('frontend/images/CAR202304060018_BMW_X5_20230406_101922704_WATERMARK.png')}}" alt=""></figure></a>
-                            </div>
-                            <div class="mycar-detail-mb">
-                                <a href="car-detail.php">
-                                    <div class="mycar-name">2023 BMW X1</div>
-                                    <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                    <div class="mycar-idcar">4กข 8113</div>
-                                </a>
-                            </div>
-                            <div class="item-mycar-detail-check item-mycar-detail">
-                                <div class="row">
-                                    <div class="col-12 col-md-6">
-                                        <a href="car-detail.php">
-                                            <div class="mycar-name">2023 BMW X1</div>
-                                            <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                            <div class="mycar-idcar">4กข 8113</div>
-                                        </a>
-                                    </div>
-                                    <div class="col-12 col-md-6 text-end">
-                                        <div class="mycar-post">วันที่ลงขาย :  16 มิ.ย. 66</div>
-                                        <div class="mycar-price-mb mycar-price">599,000.-</div>
-                                    </div>
-                                </div>
-                                <div class="mycar-boxline">
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="mycar-boxprice">
-                                                <div class="mycar-price">599,000.-</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
 
 <?php
 
@@ -166,50 +76,7 @@ $default_image = asset('frontend/images/CAR202304060018_BMW_X5_20230406_10192270
 
                         
 
-                        <!-- <div class="item-mycar">
-                            <div class="item-mycar-cover">
-                                <a href="car-detail.php"><figure><img src="{{asset('frontend/images/CAR202304060018_BMW_X5_20230406_101922704_WATERMARK.png')}}" alt=""></figure></a>
-                            </div>
-                            <div class="mycar-detail-mb">
-                                <a href="car-detail.php">
-                                    <div class="mycar-name">2023 BMW X1</div>
-                                    <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                    <div class="mycar-idcar">4กข 8113</div>
-                                </a>
-                            </div>
-                            <div class="item-mycar-detail">
-                                <div class="row">
-                                    <div class="col-12 col-md-6">
-                                        <a href="car-detail.php">
-                                            <div class="mycar-name">2023 BMW X1</div>
-                                            <div class="mycar-type">X1 2.0 sDrive18i</div>
-                                            <div class="mycar-idcar">4กข 8113</div>
-                                        </a>
-                                    </div>
-                                    <div class="col-12 col-md-6 text-end">
-                                        <div class="mycar-post">วันที่ลงขาย :  16 มิ.ย. 66</div>
-                                        <div class="mycar-expire">วันที่หมดอายุ :  16 ธ.ค. 66</div>
-                                    </div>
-                                </div>
-                                <div class="mycar-boxline">
-                                    <div class="row">
-                                        <div class="col-8 col-md-8">
-                                            <div class="mycar-boxprice">
-                                                <div class="mycar-price">599,000.-</div>
-                                                <a data-fancybox data-src="#edit-carprice" href="javascript:;" class="mycar-editprice"><i class="bi bi-pencil-square"></i> แก้ไขราคา</a>
-                                            </div>
-                                        </div>
-                                        <div class="col-4 col-md-4 text-end">
-                                            <button class="mycar-reserve"><img src="{{asset('frontend/images/icon-check.svg')}}" class="svg" alt=""> จองแล้ว</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item-mycar-button">
-                                <a href="#" class="btn-mycar btn-mycar-edit"><i class="bi bi-pencil-square"></i> แก้ไข</a>
-                                <button class="btn-mycar btn-mycar-delete button-delete"><i class="bi bi-trash3-fill"></i> ลบ</button>
-                            </div>
-                        </div> -->
+
 
 
                         <div style="display: none;" id="edit-carprice">
