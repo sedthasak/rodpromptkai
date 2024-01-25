12d96b3b92cab2b8fa7b88c9969e227bbf72ec3e
Sedthasak <nuyagoon@hotmail.com>
2024-01-25 09:59:22 +0700

    update first exterior to feature

--- resources/views/frontend/car-detail.blade.php
+++ resources/views/frontend/car-detail.blade.php
@@ -102,9 +102,11 @@ $arr_gear = array(
                         <div class="slide-wrapper">
                             <div class="tab_pdetail_thumb slide_load">
                                 <div id="sync2" class="owl-carousel owl-theme navigation-thumbs">
-                                    {{-- <div class="item cover-carthumb">
-                                        <img src="{{asset($cars->feature)}}">
-                                    </div> --}}
+                                    @if ($cars->feature != $exterior[0]->gallery)
+                                        <div class="item cover-carthumb">
+                                            <img src="{{asset($cars->feature)}}">
+                                        </div>
+                                    @endif
                                     @foreach($exterior as $index => $ext)
                                     <div class="item cover-carthumb">
                                         <img src="{{asset($ext->gallery)}}">
